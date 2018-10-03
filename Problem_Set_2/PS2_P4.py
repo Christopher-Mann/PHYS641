d99d231883263bbbe04e3a91485fbed67b8e3cc1
Christopher-Mann <chriz_010@hotmail.com>
2018-10-03 15:42:51 -0400

    Update PS2_P4.py

--- Problem_Set_2/PS2_P4.py
+++ Problem_Set_2/PS2_P4.py
@@ -4,8 +4,6 @@ Created on Fri Sep 14 16:29:45 2018
 
 @author: Chris
 """
-
-
 from __future__ import division
 import numpy as np
 import scipy as sp
@@ -19,26 +17,30 @@ from math import pi
 
 x = np.arange(0,1000)
 
+# Shape of the signal (amplitude = 1)
 def G(x):
     return np.exp(-(x-500)**2 / (2*50**2))
     
 
-
+# different correlation strenghths and lengths to explore
 a = [0.1, 0.5, 0.9]
 sig = [5, 50, 500]
 
 
 
-
+# two loops for all combinations of [a,sigma]
 for k in range(len(a)):
     for l in range(len(sig)):
-    
+        
+        #build the noise matrix based on choice of [a,sigma]
         N = np.zeros([len(x),len(x)])
         for i in range(len(x)):
             for j in range(len(x)):
                 N[i,j] = a[k]*np.exp( -(i-j)**2 / (2*sig[l]**2) )
         N = N + np.identity(len(x))*(1-a[k])
         
+        # in a classical (A^T N^-1 A) least squares fit
+        # sigma of m (amplitude) is 1/sqrt(A^T N^-1 A)
         A = G(x)
         AT = A.transpose()
         Ni = np.linalg.inv(N)
@@ -63,11 +65,6 @@ a=0.9, sig=  5 -> amp uncertainty = 0.358
 a=0.9, sig= 50 -> amp uncertainty = 0.950
 a=0.9, sig=500 -> amp uncertainty = 0.049
 
-Notes:
-
-For a given correlation strength (a) the error is largest for an intermediate
-correlation distance (sig).
-
 """
 
 
@@ -75,7 +72,7 @@ correlation distance (sig).
 """
     part b)
 
-The worst error comes from a high correlation strenght (a) mixed
+The worst error comes from a high correlation strenght (a) combined
 with a moderate correlation length (sig).  I suspect this is problematic
 because sizeable chunks of the data are tightly correlated, but not with
 other sections of the data.  It's hard to tell how the system as a whole
@@ -86,7 +83,7 @@ distance.  I suspect this is so low because sigma is comparable to the number
 of data points in the sample (500 vs. 1000).  In this case most of the data set
 is tightly correlated and therefore quite predictable.
 
-The worst cases are consistently the "clumpy" ones where the correlation lenght
+The worst cases are consistently the "clumpy" ones where the correlation length
 is bigger than just a few points, but not yet comparable to the number of data points.
 
 """
